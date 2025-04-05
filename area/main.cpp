67d0278c4d41d1d7c670dbfd7ae0afd0f9071bc0
haris2008 <hm602065@gmail.com>
2025-04-05 19:07:20 +0000

    area

---
+++ area/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int area,length,width;
+  cout << "Enter the Length of the area: ";
+  cin >> length;
+  cout << "Enter the width of the area: ";
+  cin >> width;
+  area = length * width;
+  cout << "The area is " << area << endl;
+
+    return 0;
+}
