a1219ccd050dc86a1eb1907064272c2745a25b4c
haris2008 <hm602065@gmail.com>
2025-04-07 08:46:53 +0000

    for loop

---
+++ for loop/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int nums[]{1,3,5,7,9,11,13,17,19,23};
+     for(int i = 0; i < 10;i++){
+        cout << nums[i] << endl;
+    }
+    return 0;
+}
