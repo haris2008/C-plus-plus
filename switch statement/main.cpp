f1813e872a737affabe5d08481873cbe8ad7db7e
haris2008 <hm602065@gmail.com>
2025-04-07 01:38:14 +0000

    While loopps

--- switch statement/main.cpp
+++ switch statement/main.cpp
@@ -2,8 +2,56 @@
 
 using namespace std;
 
+string getdayofweek(int dayNum){
+    string dayName;
+    switch(dayNum){
+
+    case 0:
+
+        dayName = "Sunday";
+        break;
+
+    case 1:
+
+        dayName = "Monday";
+        break;
+
+    case 2:
+
+        dayName = "Tuesday";
+        break;
+
+    case 3:
+
+        dayName = "Wednesday";
+        break;
+
+    case 4:
+
+        dayName = "Thursday";
+        break;
+
+    case 5:
+
+        dayName = "Friday";
+        break;
+
+        case 6:
+
+        dayName = "Saturday";
+        break;
+
+        default:
+            dayName = "Invalid Day Number";
+    }
+    return dayName;
+}
+
+
+
+
 int main()
 {
-
+    cout << getdayofweek(10);
     return 0;
 }
