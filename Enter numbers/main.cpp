176e62b392df2099339639e5ce3f7c4cd68d06c9
haris2008 <hm602065@gmail.com>
2025-04-17 02:52:15 +0000

    Enter numbers

---
+++ Enter numbers/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter First  Number: ";
+    cin >> n;
+    for(int i = 1; i < n; i++){
+            cout << i << " ";
+
+    }
+
+
+
+    return 0;
+}
